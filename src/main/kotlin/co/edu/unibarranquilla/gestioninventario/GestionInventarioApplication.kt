288c4ef311a757910cd18f0c4b4290605cce1659
SAM <samuelleonesh10@gmail.com>
2026-07-20 13:00:03 -0500

    Configuración inicial del proyecto

---
+++ src/main/kotlin/co/edu/unibarranquilla/gestioninventario/GestionInventarioApplication.kt
@@ -0,0 +1,11 @@
+package co.edu.unibarranquilla.gestioninventario
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GestionInventarioApplication
+
+fun main(args: Array<String>) {
+    runApplication<GestionInventarioApplication>(*args)
+}
